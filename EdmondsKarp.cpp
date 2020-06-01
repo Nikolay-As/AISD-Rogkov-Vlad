49b8c5a6a6224ee56d460b9aa02b59197b77d101
ваше имя < >
2020-06-01 16:59:26 +0300

    release

---
+++ EdmondsKarp.cpp
@@ -0,0 +1,20 @@
+﻿#include<fstream>
+
+#include"Network.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	try {
+		int source, sink;
+		ifstream input("D:\\Учеба\\АиСд\\Edmonds_Karp\\Input4.txt");
+		Network* transnet = new Network(input);
+		int MaxFlow = transnet->max_flow();
+		cout << endl << "Max Flow is " << MaxFlow << endl;
+		delete transnet;
+	}
+
+	catch (exception & ex) {
+		std::cout << ex.what();
+	}
+}
